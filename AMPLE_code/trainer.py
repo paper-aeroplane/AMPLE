b5d83f590a4f422b48411e696575378d43a08207
Daria Aksenova <84663884+paper-aeroplane@users.noreply.github.com>
2024-01-14 22:57:56 +0500

    Update trainer.py

--- AMPLE_code/trainer.py
+++ AMPLE_code/trainer.py
@@ -7,7 +7,7 @@ import torch
 from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
 from tqdm import tqdm
 
-from utils import debug
+from .utils import debug
 from modules.model import DevignModel
 
 def evaluate_loss(model, loss_function, num_batches, data_iter, cuda=False):
